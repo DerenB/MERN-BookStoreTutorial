84b0f01b5a63f768c7ddbfe47d0762ab707f62ab
DerenB <dbozerprogramming@gmail.com>
2023-09-29 18:05:52 -0400

    Put and Delete Methods

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,63 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// Route for Reading 1 book
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+        
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+// Update Book
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (!request.body.title || !request.body.author || !request.body.publishYear) {
+            return response.status(400).send({
+                message: 'Send all required fields: Title, Author, Publish Year'
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found '});
+        }
+
+        return response.status(200).send({ message: 'Book updated successfully. '});
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+// Delete a Book
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found '});
+        }
+
+        return response.status(200).send({ message: 'Book deleted successfully '});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message})
+    }
+});
+
 // Connect to database
 mongoose.connect(mongoDbUrl)
     .then(() => {
